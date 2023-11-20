e2cc50f2c8ad322e2512740697863d64cbfcdeff
Brent Ross Habawel <rosshabawel12@gmail.com>
2023-11-20 18:37:54 +0800

    added tip state

--- src/App.js
+++ src/App.js
@@ -11,12 +11,21 @@ function App() {
 
 function TipCalculator() {
   const [bill, setBill] = useState("");
+  const [percentage1, setPercentage1] = useState(0);
+  const [percentage2, setPercentage2] = useState(0);
+
+  const tip = bill * ((percentage1 + percentage2) / 2 / 100);
+  const totalBill = bill + tip;
   return (
     <div>
       <BillInput bill={bill} onSetBill={setBill} />
-      <SelectPercentage>How did you like the service?</SelectPercentage>
-      <SelectPercentage>How did your friend like the service?</SelectPercentage>
-      <Output bill={bill} />
+      <SelectPercentage percentage={percentage1} onSelect={setPercentage1}>
+        How did you like the service?
+      </SelectPercentage>
+      <SelectPercentage percentage={percentage2} onSelect={setPercentage2}>
+        How did your friend like the service?
+      </SelectPercentage>
+      <Output bill={bill} tip={tip} totalBill={totalBill} />
       <Reset />
     </div>
   );
@@ -36,11 +45,14 @@ function BillInput({ bill, onSetBill }) {
   );
 }
 
-function SelectPercentage({ children }) {
+function SelectPercentage({ children, percentage, onSelect }) {
   return (
     <div>
       <label>{children}</label>
-      <select>
+      <select
+        value={percentage}
+        onChange={(e) => onSelect(Number(e.target.value))}
+      >
         <option value="0">Dissatisfied (0%)</option>
         <option value="5">It was okay (5%)</option>
         <option value="10">It was good (10%)</option>
@@ -50,8 +62,12 @@ function SelectPercentage({ children }) {
   );
 }
 
-function Output({ bill }) {
-  return <h3>You pay X (${bill} + $B tip)</h3>;
+function Output({ bill, tip, totalBill }) {
+  return (
+    <h3>
+      You'll pay ${totalBill} (${bill} + ${tip} tip)
+    </h3>
+  );
 }
 
 function Reset() {
